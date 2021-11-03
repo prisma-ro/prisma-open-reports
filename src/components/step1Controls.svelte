87ea2358ab9c94420843207dd0d15999d32ed597
Raul-Popa <raulpopa1315@gmail.com>
2021-11-03 18:32:42 +0200

    Fix placement issues for marker modal

--- src/components/step1Controls.svelte
+++ src/components/step1Controls.svelte
@@ -4,27 +4,22 @@
 </script>
 
 <div
-  class="fixed bottom-0 md:right-32 md:top-72 z-10 flex items-end justify-center pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+  class="fixed bottom-0 md:right-5 z-10 flex items-end justify-center pt-4 px-4 pb-5 text-center sm:block"
 >
-  <!-- This element is to trick the browser into centering the modal contents. -->
-  <span
-    class="hidden sm:inline-block sm:align-middle sm:h-screen"
-    aria-hidden="true">&#8203;</span
-  >
-
   <div
-    class="inline-block align-bottom drop-shadow-2xl border-prisma-purple-500 border-2 bg-white rounded-lg text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+    class="inline-block align-bottom drop-shadow-2xl border-prisma-purple-500 border-2 bg-white rounded-lg text-left overflow-hidden transform transition-all sm:my-2 sm:align-middle sm:max-w-lg sm:w-full"
   >
     <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
       <div class="sm:flex sm:items-start">
-        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-          <p class="text-sm md:text-base text-gray-600">
-            Dacă vrei să schimbi locația incidentului, doar apasă din nou pe hartă!
+        <div class="mt-3 sm:mt-0 sm:ml-4 sm:text-left">
+          <p class="text-sm md:text-base text-gray-600 text-center">
+            Dacă vrei să schimbi locația incidentului, doar apasă din nou pe
+            hartă!
           </p>
         </div>
       </div>
     </div>
-    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row sm:justify-center">
       <button
         type="button"
         class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-prisma-purple-500 text-base font-medium text-white hover:bg-prisma-purple-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-prisma-purple-300 sm:ml-3 sm:w-auto sm:text-sm"

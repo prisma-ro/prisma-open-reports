8ae3b71e1e65382595a76d1e0bb128dfda142218
David Pescariu <davidpescariu12@gmail.com>
2022-02-11 17:58:56 +0200

    feat: Update Agreement + show "important message"

--- src/components/stepOne.svelte
+++ src/components/stepOne.svelte
@@ -16,22 +16,36 @@
       </div>
     </div>
   </div>
-  <div
-    class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row sm:justify-center"
-  >
-    <button
-      type="button"
-      class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-prisma-purple-500 text-base font-medium text-white hover:bg-prisma-purple-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-prisma-purple-300 sm:ml-3 sm:w-auto sm:text-sm"
-      on:click={okCallback}
-    >
-      Continuă!
-    </button>
-    <button
-      type="button"
-      class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-      on:click={cancelCallback}
-    >
-      Șterge marker-ul
-    </button>
+  <div class="bg-gray-50 px-4 py-3 sm:px-6 flex flex-col justify-center">
+    <div class="sm:flex sm:flex-row sm:justify-center">
+      <button
+        type="button"
+        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-prisma-purple-500 text-base font-medium text-white hover:bg-prisma-purple-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-prisma-purple-300 sm:ml-3 sm:w-auto sm:text-sm"
+        on:click={okCallback}
+      >
+        Continuă!
+      </button>
+      <button
+        type="button"
+        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+        on:click={cancelCallback}
+      >
+        Șterge marker-ul
+      </button>
+    </div>
+    <div class="text-gray-600 text-center leading-4 mt-2">
+      <small>
+        <a
+          href="docs/Acord-Prelucrarea-Datelor-Furnizate-pe-Platforma-Open-Reports.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Important: Continuarea reprezintă acceptul dvs. al
+          <span class="whitespace-nowrap">
+            Acordului de Prelucrare a Datelor Furnizate
+          </span>
+        </a>.
+      </small>
+    </div>
   </div>
 </StepModal>

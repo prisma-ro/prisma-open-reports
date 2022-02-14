bcf265c96eaa1bcdfe63101cf115ab641ac89e2f
David Pescariu <davidpescariu12@gmail.com>
2022-02-14 17:30:12 +0200

    fix: (TEMP) Center on Romania

--- src/screens/map.svelte
+++ src/screens/map.svelte
@@ -124,12 +124,12 @@
   <Map
     accessToken="pk.eyJ1IjoicHJpc21hLWRhdmlkcCIsImEiOiJja3ZlMGs3bm00N3NyMm9scHYxcDcwMW5lIn0.al4e5xd-S95t1srowwoWXw"
     style="mapbox://styles/prisma-davidp/ckt6xnv4c1ut617ns7444v18e"
-    center={[23.591675, 46.770764]}
-    zoom="13"
+    center={[24.849442, 46.069881]}
+    zoom="5.5"
     bind:this={mapComponent}
     on:click={onMapClick}
     options={{
-      minZoom: 6,
+      minZoom: 5,
       maxBounds: [
         [19, 43],
         [30, 49],
